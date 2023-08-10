acc70ec4519f349d23c17bc4966cf03c194da406
Donatas Noreika <donoras@gmail.com>
2023-08-10 11:35:50 +0300

    +Fiksuojama ir keičiama tanko kryptis

--- tankas.py
+++ tankas.py
@@ -3,19 +3,25 @@ class Tankas:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.kryptis = "Šiaurė"
 
     def pirmyn(self):
         self.y += 1
+        self.kryptis = "Šiaurė"
 
     def atgal(self):
         self.y -= 1
+        self.kryptis = "Pietūs"
 
     def kairen(self):
         self.x -= 1
+        self.kryptis = "Vakarai"
 
     def desinen(self):
         self.x += 1
+        self.kryptis = "Rytai"
 
     def info(self):
         print("x:", self.x)
         print("y:", self.y)
+        print("Kryptis:", self.kryptis)
